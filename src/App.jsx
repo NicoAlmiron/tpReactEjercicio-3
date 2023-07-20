3eb4c51fa41c3a33913aa97f087c48ee62d5d53f
Nico Almiron <almiron.nicolas16@gmail.com>
2023-07-20 17:51:12 -0300

    se finalizo el ejercicio

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,10 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Texto from "./components/Texto";
 
-const textoModificado = "my friend!";
-
 function App() {
   return (
     <>
-      <Texto textoModificado={textoModificado}></Texto>
+      <Texto textoModificado={"my friend"}></Texto>
     </>
   );
 }
